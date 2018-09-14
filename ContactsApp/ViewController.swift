0a019739fe734fbd401461ea51692b857808e09e
Taehyeon (Jeff) Eom <jeffeom@gmail.com>
2018-09-13 21:20:23 -0700

    Initial Commit

---
+++ ContactsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by Jeff Eom on 2018-09-13.
+//  Copyright © 2018 Jeff Eom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
